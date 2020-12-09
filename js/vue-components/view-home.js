4ae76f897024818de737a5612e2ad5754e36acf8
Gabriele <gabriele.97@hotmail.it>
2020-12-09 17:54:51 +0100

    Modifica template
    Modificata la struttura di index.html.
    Prime modifiche alla vista home

--- js/vue-components/view-home.js
+++ js/vue-components/view-home.js
@@ -1,7 +1,42 @@
 var ViewHome = Vue.component('view-home', {
   data: function () {
-    return {
-    }
+    return {}
   },
-  template: '<div>Ciao <button @click="$emit(\'change-view-event\',\'view-challenges\')" type="submit">BACK TO PREVIOUS VIEW</button></div>'
+  template: `
+    <div>
+      <div class="card-title">
+      </div>
+      <div class="card-body">
+        <div class="row h-100">
+            <div class="col-12">
+              <div class="row">
+                <div class="col-12 my-auto">
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate nibh a urna convallis finibus.
+                         Etiam in lacus nibh. Nam euismod dignissim sapien, ut semper dolor. Integer imperdiet auctor libero, nec consequat sapien imperdiet ut.
+                          Mauris tincidunt augue magna, nec convallis diam pulvinar ut.
+                    </div>
+                </div>
+              </div>
+              <div class="row h-30">
+                  <div class="col-12 text-center my-auto">
+                        <button @click="$emit('change-view-event','view-challenges')" class="btn btn-primary btn-round">
+                            <h1 class="text-white blinking">START GAME</h1>
+                         </button>
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col-12 my-auto">
+
+                  </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="card-footer">
+      </div>
+    </div>
+  `
 })
+
+// <button @click="$emit('change-view-event','view-challenges')" type="submit">BACK TO PREVIOUS VIEW</button> `
