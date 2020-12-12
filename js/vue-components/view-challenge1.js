0cf36f8d5de2f41b3b8d6988dc0e4452f3fbaff0
Gabriele <gabriele.97@hotmail.it>
2020-12-12 17:47:32 +0100

    update

--- js/vue-components/view-challenge1.js
+++ js/vue-components/view-challenge1.js
@@ -7,12 +7,12 @@ var ViewChallenge1 = Vue.component('view-challenge1', {
   },
   template: `
     <div class="h-100">
-        <div class="card-header card-header-icon card-header-info" style="height: 10%">
+        <div class="card-header card-header-icon card-header-info">
             <div class="card-text">
                 <h4 class="card-title text-xl-center">Challenge 1</h4>
             </div>
         </div>
-        <div class="card-body" style="height: 90%">
+        <div class="card-body h-75">
             <div class="row h-100">
                 <div class="col-12 h-100">
                     <div class="row h-25 text-center">
@@ -29,24 +29,20 @@ var ViewChallenge1 = Vue.component('view-challenge1', {
                     <div class="row h-75">
                         <div class="col-12 h-100 text-center">
                             <div class="row h-25">
-                                <div class="col-12 h-100 align-middle">
-                                    <h4>{{ challenge.question.question }}</h4>
+                                <div class="col-12 h-100">
+                                    <div class="align-middle">
+                                        <h4>{{ challenge.question.question }}</h4>
+                                    </div>
                                 </div>
+
                             </div>
                             <div class="row h-75">
-                               <div class="col-12 h-100 text-center">
-                                  <ul class="nav nav-pills nav-pills-icons nav-pills-warning h-100 w-100 d-flex mx-auto text-center" style="justify-content: space-between" role="tablist">
-                                      <!--
-                                                      color-classes: "nav-pills-primary", "nav-pills-info", "nav-pills-success", "nav-pills-warning","nav-pills-danger"
-                                                  -->
-                                      <li v-for="(option, index) in challenge.question.options" class="nav-item">
-                                        <div v-if="index == 0" @click="$emit('change-language-event','EN')" class="btn btn-block btn-link nav-link active" role="tab" data-toggle="tab" style="max-width: 20%">
-                                            <div>{{option}}</div>
+                               <div class="col-12 h-100">
+                                  <ul class="ml-0 nav nav-pills justify-content-center nav-pills-warning w-100 h-100"  role="tablist">
+                                      <li v-for="(option, index) in challenge.question.options" class="nav-item w-25 h-50">
+                                        <div @click="$emit('change-language-event','EN')" class="btn-link h-100 w-100 nav-link"  data-toggle="tab">
+                                            <div class="">{{option}}</div>
                                         </div>
-                                        <div v-else @click="$emit('change-language-event','EN')" class="btn btn-block btn-link nav-link" role="tab" data-toggle="tab" style="max-width: 20%">
-                                            <div>{{option}}</div>
-                                        </div>
-
                                       </li>
 
                                   </ul>
