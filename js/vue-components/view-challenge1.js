d4b0fe719ab19938370bc05c11e68b677a2d7c6a
Gabriele <gabriele.97@hotmail.it>
2020-12-13 12:14:27 +0100

    Layout changes

--- js/vue-components/view-challenge1.js
+++ js/vue-components/view-challenge1.js
@@ -12,51 +12,48 @@ var ViewChallenge1 = Vue.component('view-challenge1', {
                 <h4 class="card-title text-xl-center">Challenge 1</h4>
             </div>
         </div>
-        <div class="card-body" style="height: 90%">
-            <div class="row h-100">
-                <div class="col-12 h-100">
-                    <div class="row h-25 text-center">
-                        <div class="col-12 h-100">
-                            <h4>{{ challenge.description }}</h4>
-                            <hr>
-                            <h6>YOU ARE PLAYING WITH THE WORD</h6>
-                            <h3><strong class="text-info">{{ challenge.word }}</strong></h3>
-                        </div>
-                    </div>
-                    <div class="row h-75">
-                        <div class="col-12 h-100 text-center">
-                            <div class="row h-25">
-                                <div class="col-12 h-100">
-                                    <div class="align-middle">
-                                        <h3>{{ challenge.question.question }}</h3>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="row h-75">
-                               <div class="col-12 h-100">
-                                  <ul class="ml-0 nav nav-pills justify-content-center nav-pills-warning w-100 h-100"  role="tablist">
-                                      <li v-for="(option, index) in challenge.question.options" class="nav-item w-25 h-100">
-                                        <div @click="$emit('change-language-event','EN')" class="btn-link h-100 w-100 nav-link"  data-toggle="tab">
-                                            <table class="h-100">
-                                              <tbody>
-                                                <tr>
-                                                  <td class="align-middle">{{option}}</td>
-                                                </tr>
-                                              </tbody>
-                                            </table>
-                                        </div>
-                                      </li>
-
-                                  </ul>
-                               </div>
-                            </div>
-
-                        </div>
-                    </div>
-
+        <div class="card-body d-flex flex-column align-items-center">
+            <div class="row  mt-3">
+                <div class="col-12">
+                    <h4>{{ challenge.description }}</h4>
+                    <hr>
                 </div>
             </div>
+            <div class="row mt-auto mb-auto text-center">
+                <div class="col-12">
+                    <div class="row">
+                      <div class="col-12">
+                          <h6>YOU ARE PLAYING WITH THE WORD</h6>
+                          <h3><strong class="text-info">{{ challenge.word }}</strong></h3>
+                      </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-12">
+                            <h3>{{ challenge.question.question }}</h3>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-12 text-center">
+                            <ul class="nav nav-pills justify-content-center nav-pills-warning w-100"  role="tablist">
+                                <li v-for="(option, index) in challenge.question.options" class="nav-item w-25">
+                                  <div @click="$emit('change-language-event','EN')" class="btn-link w-100 nav-link"  data-toggle="tab">
+                                      <table class="h-100">
+                                        <tbody>
+                                          <tr>
+                                            <td class="align-middle">{{option}}</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                  </div>
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
--- js/vue-components/view-home.js
+++ js/vue-components/view-home.js
@@ -3,85 +3,81 @@ var ViewHome = Vue.component('view-home', {
     return {}
   },
   template: `
-    <div  class="card mt-5 mx-auto d-flex bg-dark" style="min-height: 80vh; width: 60vw">
+    <div  class="card mt-5 mx-auto d-flex " style="min-height: 80vh; width: 60vw">
       <div class="card-header card-header-icon card-header-info">
         <div class="card-text">
           <h4 class="card-title text-xl-center">Main menu</h4>
         </div>
       </div>
-      <div class="card-body align-items-center d-flex justify-content-center bg-success h-100">
-        <div class="row h-100">
-            <div class="col-12 h-100 my-auto">
-              <div class="row h-25">
-                <div class="col-12 text-center my-auto">
-                    <h3>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis eleifend sem. Pellentesque posuere quam nisi, et condimentum neque interdum sed.
-                    </h3>
-                </div>
-              </div>
-              <div class="row h-25">
-                  <div class="col-12 my-auto mx-auto text-center">
-                      <ul class="nav nav-pills nav-pills-icons nav-pills-info w-100 mx-auto text-center" role="tablist">
-                          <!--
-                                          color-classes: "nav-pills-primary", "nav-pills-info", "nav-pills-success", "nav-pills-warning","nav-pills-danger"
-                                      -->
-                          <li class="nav-item ml-auto" >
-                            <div @click="$emit('change-language-event','EN')" class="btn btn-link nav-link active" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
-                                  <img class="card-img-top img-circle mx-auto"
-                                      src="img/country_flags/united-kingdom.svg"
-                                      alt="Card image cap"
-                                      style="max-height: 100%; max-width: 100%;"
-                                  >
-                            </div>
-                          </li>
-                          <li class="nav-item" >
-                            <div @click="$emit('change-language-event','IT')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
-                                  <img class="card-img-top img-circle mx-auto"
-                                      src="img/country_flags/italy.svg"
-                                      alt="Card image cap"
-                                      style="max-height: 100%; max-width: 100%;"
-                                  >
-                            </div>
-                          </li>
-                          <li class="nav-item" >
-                            <div @click="$emit('change-language-event','FR')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
-                                  <img class="card-img-top img-circle mx-auto"
-                                      src="img/country_flags/france.svg"
-                                      alt="Card image cap"
-                                      style="max-height: 100%; max-width: 100%;"
-                                  >
-                            </div>
-                          </li>
-                          <li class="nav-item" >
-                            <div @click="$emit('change-language-event','ES')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
-                                  <img class="card-img-top img-circle mx-auto"
-                                      src="img/country_flags/spain.svg"
-                                      alt="Card image cap"
-                                      style="max-height: 100%; max-width: 100%;"
-                                  >
-                            </div>
-                          </li>
-                          <li class="nav-item mr-auto" >
-                            <div @click="$emit('change-language-event','DE')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
-                                  <img class="card-img-top img-circle mx-auto"
-                                      src="img/country_flags/germany.svg"
-                                      alt="Card image cap"
-                                      style="max-height: 100%; max-width: 100%;"
-                                  >
-                            </div>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
-              <div class="row h-50">
-                  <div class="col-12 my-auto text-center">
-                        <button @click="$emit('change-view-event','view-challenge1')" class="btn btn-info btn-round">
-                            <h1 class="text-white blinking">START GAME</h1>
-                         </button>
-                  </div>
-              </div>
+      <div class="card-body d-flex flex-column justify-content-around">
+          <div class="row">
+            <div class="col-12 text-center align-self-center">
+                <h3>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis eleifend sem. Pellentesque posuere quam nisi, et condimentum neque interdum sed.
+                </h3>
             </div>
-        </div>
+          </div>
+          <div class="row">
+              <div class="col-12 my-auto mx-auto text-center">
+                  <ul class="nav nav-pills nav-pills-icons nav-pills-info w-100 mx-auto text-center" role="tablist">
+                      <!--
+                                      color-classes: "nav-pills-primary", "nav-pills-info", "nav-pills-success", "nav-pills-warning","nav-pills-danger"
+                                  -->
+                      <li class="nav-item ml-auto" >
+                        <div @click="$emit('change-language-event','EN')" class="btn btn-link nav-link active" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
+                              <img class="card-img-top img-circle mx-auto"
+                                  src="img/country_flags/united-kingdom.svg"
+                                  alt="Card image cap"
+                                  style="max-height: 100%; max-width: 100%;"
+                              >
+                        </div>
+                      </li>
+                      <li class="nav-item" >
+                        <div @click="$emit('change-language-event','IT')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
+                              <img class="card-img-top img-circle mx-auto"
+                                  src="img/country_flags/italy.svg"
+                                  alt="Card image cap"
+                                  style="max-height: 100%; max-width: 100%;"
+                              >
+                        </div>
+                      </li>
+                      <li class="nav-item" >
+                        <div @click="$emit('change-language-event','FR')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
+                              <img class="card-img-top img-circle mx-auto"
+                                  src="img/country_flags/france.svg"
+                                  alt="Card image cap"
+                                  style="max-height: 100%; max-width: 100%;"
+                              >
+                        </div>
+                      </li>
+                      <li class="nav-item" >
+                        <div @click="$emit('change-language-event','ES')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
+                              <img class="card-img-top img-circle mx-auto"
+                                  src="img/country_flags/spain.svg"
+                                  alt="Card image cap"
+                                  style="max-height: 100%; max-width: 100%;"
+                              >
+                        </div>
+                      </li>
+                      <li class="nav-item mr-auto" >
+                        <div @click="$emit('change-language-event','DE')" class="btn btn-link nav-link" role="tab" data-toggle="tab" style="height: 60px; width: 60px;">
+                              <img class="card-img-top img-circle mx-auto"
+                                  src="img/country_flags/germany.svg"
+                                  alt="Card image cap"
+                                  style="max-height: 100%; max-width: 100%;"
+                              >
+                        </div>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-12 my-auto text-center">
+                    <button @click="$emit('change-view-event','view-challenge1')" class="btn btn-info btn-round">
+                        <h1 class="text-white blinking">START GAME</h1>
+                     </button>
+              </div>
+          </div>
       </div>
       <div class="card-footer">
       </div>
