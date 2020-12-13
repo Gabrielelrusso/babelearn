8aa95a601ec3a125c0fec5917f37a4611d1c358d
mrispoli97 <mariorispoli97@gmail.com>
2020-12-13 11:33:10 +0100

    view-challenges update

--- js/vue-components/view-challenges.js
+++ js/vue-components/view-challenges.js
@@ -22,9 +22,9 @@ var ViewChallenges = Vue.component('view-challenges', {
                 </div>
             </div>
         </div>
-        <div class="card-body h-100 my-3">
+        <div class="card-body h-100 align-items-center d-flex justify-content-center">
             <div class="row h-100">
-                <div class="col-12 my-auto h-100">
+                <div class="col-12 h-100">
                     <div class="row h-75">
                         <div class="col-2 h-100 my-auto"></div>
                         <div class="col-8 h-100 my-auto">
@@ -37,7 +37,7 @@ var ViewChallenges = Vue.component('view-challenges', {
                                 </div>
                                 <div class="col-10 h-100 my-auto mx-auto">
                                     <div id="carouselExampleIndicators" class="carousel slide h-100 my-auto" data-ride="carousel">
-                                        <div class="row" style="height: 90%">
+                                        <div class="row h-75">
                                             <div class="col h-100 my-auto">
                                                 <div class="carousel-inner h-100 my-auto">
                                                     <div class="carousel-item active h-100 mx-auto my-auto">
@@ -63,35 +63,51 @@ var ViewChallenges = Vue.component('view-challenges', {
                                                         </div>
                                                     </div>
                                                     <div class="carousel-item h-100 mx-auto my-auto">
-                                                        <div class="card h-100 mx-auto my-auto" role="tab" data-toggle="tab" style="box-shadow: none">
-                                                            <img class="card-img-top mx-auto"
-                                                                src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"
-                                                                alt="Card image cap"
-                                                                style="max-height: 120px; max-width: 120px;"
-                                                            >
-                                                            <h5 class="card-title text-center text-info">Challenge Two</h5>
-                                                            <div class="card-body">
-                                                                <div class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                                        <div class="card h-100 mx-auto" role="tab" data-toggle="tab" style="box-shadow: none">
+                                                            <div class="d-flex align-items-center h-50">
+                                                                <img class="card-img-top mx-auto my-auto"
+                                                                    src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"
+                                                                    alt="Card image cap"
+                                                                    style="max-height: 120px; max-width: 120px;"
+                                                                >
+
                                                             </div>
+                                                            <div class="d-flex align-items-center justify-content-center h-25">
+                                                                <h5 class="card-title text-info d-block">Challenge Two</h5>
+                                                            </div>
+                                                            <div class="d-flex align-items-center h-25">
+                                                                <div class="card-body d-block">
+                                                                    <div class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                                                </div>
+                                                            </div>
+
+
                                                         </div>
                                                     </div>
                                                     <div class="carousel-item h-100 mx-auto my-auto">
-                                                        <div class="card h-100 mx-auto my-auto" role="tab" data-toggle="tab" style="box-shadow: none">
-                                                            <img class="card-img-top mx-auto"
-                                                                src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"
-                                                                alt="Card image cap"
-                                                                style="max-height: 120px; max-width: 120px;"
-                                                            >
-                                                            <h5 class="card-title text-center text-info">Challenge Three</h5>
-                                                            <div class="card-body">
-                                                                <div class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                                        <div class="card h-100 mx-auto" role="tab" data-toggle="tab" style="box-shadow: none">
+                                                            <div class="d-flex align-items-center h-50">
+                                                                <img class="card-img-top mx-auto my-auto"
+                                                                    src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"
+                                                                    alt="Card image cap"
+                                                                    style="max-height: 120px; max-width: 120px;"
+                                                                >
+
+                                                            </div>
+                                                            <div class="d-flex align-items-center justify-content-center h-25">
+                                                                <h5 class="card-title text-info d-block">Challenge Three</h5>
+                                                            </div>
+                                                            <div class="d-flex align-items-center h-25">
+                                                                <div class="card-body d-block">
+                                                                    <div class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="row" style="height: 10%">
+                                        <div class="row h-25">
                                             <div class="col h-100 my-auto">
                                                 <ol class="carousel-indicators my-auto">
                                                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -109,53 +125,6 @@ var ViewChallenges = Vue.component('view-challenges', {
                                     </a>
                                 </div>
                             </div>
-
-<!--                            <ul class="nav nav-pills nav-pills-icons nav-pills-info mx-auto h-100 d-flex" role="tablist" style="justify-content: space-between">-->
-<!--                        &lt;!&ndash;-->
-<!--                                        color-classes: "nav-pills-primary", "nav-pills-info", "nav-pills-success", "nav-pills-warning","nav-pills-danger"-->
-<!--                                    &ndash;&gt;-->
-<!--                                <li class="nav-item h-100" style="max-width: 30%;">-->
-<!--                                    <div class="card nav-link active h-100" role="tab" data-toggle="tab">-->
-<!--                                        <img class="card-img-top img-circle mx-auto"-->
-<!--                                            src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"-->
-<!--                                            alt="Card image cap"-->
-<!--                                            style="max-height: 60px; max-width: 60px; margin-top: -40px"-->
-<!--                                        >-->
-<!--                                        Challenge One-->
-<!--                                        <div class="card-body">-->
-<!--                                            <div class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </li>-->
-<!--                                <li class="nav-item h-100" style="max-width: 30%;">-->
-<!--                                    <div class="card nav-link h-100" role="tab" data-toggle="tab">-->
-<!--                                        <img class="card-img-top img-circle mx-auto"-->
-<!--                                            src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"-->
-<!--                                            alt="Card image cap"-->
-<!--                                            style="max-height: 60px; max-width: 60px; margin-top: -40px"-->
-<!--                                        >-->
-<!--                                        Challenge Two-->
-<!--                                        <div class="card-body">-->
-<!--                                            <div class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </li>-->
-<!--                                <li class="nav-item h-100" style="max-width: 30%;">-->
-<!--                                    <div class="card nav-link h-100" role="tab" data-toggle="tab">-->
-<!--                                        <img class="card-img-top img-circle mx-auto"-->
-<!--                                            src="https://www.flaticon.com/svg/static/icons/svg/3869/3869355.svg"-->
-<!--                                            alt="Card image cap"-->
-<!--                                            style="max-height: 60px; max-width: 60px; margin-top: -40px"-->
-<!--                                        >-->
-<!--                                        Challenge Three-->
-<!--                                        <div class="card-body">-->
-<!--                                            <div class="card-text">-->
-<!--                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.-->
-<!--                                            </div>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </li>-->
-<!--                            </ul>-->
                         </div>
                         <div class="col-2 h-100 my-auto"></div>
                     </div>
