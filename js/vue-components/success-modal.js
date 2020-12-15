9b51fd6b061054a2f8fbe56a238ca4037cfab750
Gabriele <gabriele.97@hotmail.it>
2020-12-15 18:22:11 +0100

    Logica success modal

--- js/vue-components/success-modal.js
+++ js/vue-components/success-modal.js
@@ -8,20 +8,15 @@ var SuccessModal = Vue.component('success-modal',{
           <div class="modal-content text-center">
             <div class="modal-header bg-success text-light text-center d-flex flex-row flex-wrap align-content-center">
               <h2 class="text-center" id="successModalLabel">BRAVO!</h2>
-<!--              <img class="img-fluid"-->
-<!--                  src="img/doggos/happy-doggo.png"-->
-<!--                  alt="Bravo doggo!"-->
-<!--                  style="max-width: 150px;"-->
-<!--              >-->
             </div>
             <div class="modal-body offset-3 col-6 d-flex flex-column justify-content-around">
-              <button @click="$emit('change-view-event','view-home')" class="btn btn-info btn-round">
+              <button @click="$emit('go-to-main-menu-event')" class="btn btn-info btn-round" data-dismiss="modal">
                 MAIN MENU
               </button>
-              <button @click="$emit('change-view-event','view-home')" class="btn btn-warning btn-round">
+              <button @click="$emit('go-to-challenges-menu-event')" class="btn btn-warning btn-round" data-dismiss="modal">
                 CHANGE CHALLENGE
               </button>
-              <button @click="$emit('change-view-event','view-home')" class="btn btn-success btn-round mt-5">
+              <button @click="$emit('play-again-event')" class="btn btn-success btn-round mt-5" data-dismiss="modal">
                 AGAIN!
               </button>
             </div>
