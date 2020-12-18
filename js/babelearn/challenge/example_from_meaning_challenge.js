31133d0fb1d58d05c7bd535a82adb12bcd198d49
mrispoli97 <mariorispoli97@gmail.com>
2020-12-18 10:18:53 +0100

    Update example_from_meaning_challenge.js

---
+++ js/babelearn/challenge/example_from_meaning_challenge.js
@@ -0,0 +1,21 @@
+import {Challenge} from './challenge.js'
+import {SemanticWordDescription} from "../babelnet_interface/semantic_api/semantic_api.js";
+
+export class ExampleFromMeaningChallenge extends Challenge{
+    constructor(word, wordLang) {
+        const description = "";
+        super(word, wordLang, null, description);
+
+
+    }
+
+    async generate(){
+        this.semanticWordDescription = new SemanticWordDescription(this.getWord(), this.getWordLang(), [this.getWordLang()], 0, null);
+        this.semanticWordDescription.initialize().then((res) => {
+          let solution = this.semanticWordDescription.getExamples(this.getWordLang())[0];
+          let gloss = this.semanticWordDescription.getMeaning(this.getWordLang());
+        });
+    }
+
+
+}
