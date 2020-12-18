fc41ba81a6c7782e172ad4319c92b77bbfc80cea
mrispoli97 <mariorispoli97@gmail.com>
2020-12-18 12:46:39 +0100

    Update example_from_meaning_challenge.js

--- js/babelearn/challenge/example_from_meaning_challenge.js
+++ js/babelearn/challenge/example_from_meaning_challenge.js
@@ -3,23 +3,62 @@ import {SemanticWordDescription} from "../babelnet_interface/semantic_api/semant
 import {SemanticSentenceDescription} from "../babelnet_interface/semantic_api/semantic_api.js";
 
 export class ExampleFromMeaningChallenge extends Challenge{
-    constructor(word, wordLang) {
+    constructor(word, wordLang, gameLang) {
         const description = "";
-        super(word, wordLang, null, description);
+        super(word, wordLang, gameLang,null, description);
+        this.semanticWordDescription = null;
     }
 
     async generate(){
-        this.semanticWordDescription = new SemanticWordDescription(this.getWord(), this.getWordLang(), [this.getWordLang()], 0, null);
-        this.semanticWordDescription.initialize().then((res) => {
-          let solution = this.semanticWordDescription.getExamples(this.getWordLang())[0];
-          let gloss = this.semanticWordDescription.getMeaning(this.getWordLang());
+        this.semanticWordDescription = new SemanticWordDescription(this.getWord(), this.getWordLang(), [this.getGameLang()], null);
+
+        await this.semanticWordDescription.initialize().then((res) => {
+          let hasSolution = false;
+          let hasGloss = false;
+          let solution = "";
+          let gloss = "";
+
+          do{
+            solution = this.semanticWordDescription.getExamples(this.getWordLang())[0];
+            if(solution.length > 0){
+              hasSolution = true;
+            }
+            else{
+              hasSolution = false;
+            }
+
+            gloss = this.semanticWordDescription.getMeaning(this.getWordLang());
+            if( gloss.length > 0){
+              hasGloss = true;
+            }
+            else{
+              hasGloss = false;
+            }
+
+            if(!hasSolution || !hasGloss){
+              if(this.semanticWordDescription.hasAnotherMeaning()){
+                this.semanticWordDescription.nextMeaning();
+              }
+            }
+
+          }while(!hasSolution || !hasGloss);
+
           this.setSolution(solution);
           this.setExerciseMain(gloss);
         });
     }
 
-    guess(answer) {
+    async guess(answer) {
+        let semanticSentenceDescription = new SemanticSentenceDescription(answer, this.getGameLang());
+        let userSemanticWordDescription = null;
+        await semanticSentenceDescription.initialize().then((res) => {
+          userSemanticWordDescription = semanticSentenceDescription.getSemanticWordDescription(this.getWord(), [this.getGameLang()]);
+        });
 
+        await userSemanticWordDescription.initialize().then((res) => {
+        });
+
+        return this.semanticWordDescription.checkForEquality(userSemanticWordDescription);
     }
 
 
