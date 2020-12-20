cc2f48b119b24e1ace1505721bd240e7a36639c3
fchiaro <52402131+fchiaro@users.noreply.github.com>
2020-12-20 13:17:46 +0100

    Implemented challenge

---
+++ js/babelearn/challenge/four_images_one_word_challenge.js
@@ -0,0 +1,68 @@
+/*jshint esversion: 8 */ 
+
+import {Challenge, ChallengeBuildFailedError} from './challenge.js';
+import {SemanticWordDescription} from '../babelnet_interface/semantic_api/semantic_api.js';
+
+/**
+ * Challenge 4.
+ */
+export class FourImagesOneWordChallenge extends Challenge{
+    /**
+     * 
+     * 
+     * @param {*} word 
+     * @param {*} wordLang 
+     * @param {*} gameLang 
+     * @throws {TypeError} if word or wordLang are not specified.
+     */
+    constructor(word, wordLang, gameLang){
+        if(word == null || wordLang == null || gameLang == null){
+            throw new TypeError('Word, her language and the game language must be specified');
+        }
+
+        var description = 'Look at the four images and identify the word they represent.';
+        super(word, wordLang, gameLang, null, description);
+
+        this.gameImages = null; // null instead of empty to distinguish not initialized from no images available
+        this.NUM_GAME_IMAGES = 4;
+    }
+
+    async generate(){
+        this.gameWordSemanticDescription = new SemanticWordDescription(this.getWord(), this.getWordLang, [this.getGameLang], null);
+        var images = null;
+        await this.gameWordSemanticDescription.initialize().then((res) => {
+            images = this.gameWordSemanticDescription.getImages(); // return value is always != null
+            while(
+                    images.length < this.NUM_GAME_IMAGES && 
+                    this.gameWordSemanticDescription.hasAnotherMeaning()
+                ){
+                    this.gameWordSemanticDescription.nextMeaning();
+                    images = this.gameWordSemanticDescription.getImages();
+            }
+        });
+
+        if(images.length < this.NUM_GAME_IMAGES){
+            throw new ChallengeBuildFailedError('Unable to build FourImagesOneWordChallenge');
+        }
+
+        var start = Math.round(Math.random()*images.length) - this.NUM_GAME_IMAGES;
+        // if there is only the number of images needed, start could be less than zero
+        if (start < 0){
+            start = 0;
+        }
+
+        this.gameImages = images.slice(start, start+this.NUM_GAME_IMAGES); // slice() excludes upper limit
+
+        /**
+         * Set one of the possible solutions, e.g. to eventually show it to the user, but guess(answer) method will
+         * also accept synonyms of the game word. 
+         */
+        this.setSolution(this.gameWordSemanticDescription.getLemma(this.getGameLang()));
+    }
+
+    async guess(answer){
+        // Bring everything to lower case since 'answer' come from front-end
+        var compatible = await this.gameWordSemanticDescription.checkForCompatibility(answer, this.getGameLang());
+        return compatible;
+    }
+}
